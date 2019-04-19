45c2342af143d06363b77a660429fbc32bc4ca1c
Virat Patel <21.virat@gmail.com>
2019-04-19 14:13:06 +0530

    github page fix

---
+++ precache-manifest.8d7ee48b7fe877589d46a8f30395d585.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed6eedd3b8d73feb6451",
+    "url": "/static/js/main.f9dca85c.chunk.js"
+  },
+  {
+    "revision": "0f724051b024c62ab133",
+    "url": "/static/js/2.16e83114.chunk.js"
+  },
+  {
+    "revision": "ed6eedd3b8d73feb6451",
+    "url": "/static/css/main.0962a0c6.chunk.css"
+  },
+  {
+    "revision": "5706c8b9ddbaf0e630571c8b14371d11",
+    "url": "/index.html"
+  }
+];
